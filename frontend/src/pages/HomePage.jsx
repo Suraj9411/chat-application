ac955d3734ccc04e71edbd65096183175dfd09ab
Suraj Singh Rawat <91652123+Suraj9411@users.noreply.github.com>
2025-07-04 01:35:06 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,4 @@
-
 import { useChatStore } from "../store/useChatStore";
-
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
@@ -9,12 +7,17 @@ const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
-            <Sidebar />
+    <div className="min-h-screen bg-base-200 pt-20 px-4">
 
+      <div className="mx-auto max-w-5xl h-[calc(100vh-6rem)] bg-base-100 rounded-lg shadow-lg overflow-hidden w-full">
+        <div className="flex flex-col lg:flex-row h-full">
+          {/* Sidebar - horizontal on mobile, vertical on desktop */}
+          <div className="w-full lg:w-1/3 h-20 lg:h-full border-b lg:border-b-0 lg:border-r border-base-300">
+            <Sidebar />
+          </div>
+
+          {/* Chat area */}
+          <div className="flex-1 overflow-auto">
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
           </div>
         </div>
@@ -22,4 +25,5 @@ const HomePage = () => {
     </div>
   );
 };
+
 export default HomePage;
